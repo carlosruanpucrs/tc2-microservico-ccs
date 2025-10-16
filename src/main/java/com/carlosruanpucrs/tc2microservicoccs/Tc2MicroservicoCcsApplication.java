86f904d70b33f7174b3813f57a1d5bc2b0e48681
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-10-16 01:10:15 -0300

    projeto criado

---
+++ src/main/java/com/carlosruanpucrs/tc2microservicoccs/Tc2MicroservicoCcsApplication.java
@@ -0,0 +1,13 @@
+package com.carlosruanpucrs.tc2microservicoccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tc2MicroservicoCcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tc2MicroservicoCcsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlosruanpucrs/tc2microservicoccs/Tc2MicroservicoCcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosruanpucrs.tc2microservicoccs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tc2MicroservicoCcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
