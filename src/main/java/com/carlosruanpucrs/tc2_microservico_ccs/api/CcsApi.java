a1cd50ff49f45e8fd4eb6939ad5a30e59f1b4c28
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-10-16 01:34:20 -0300

    fix/notificacao-abertura-conta-ccs: ajuste api adicionado anotacao request body

--- src/main/java/com/carlosruanpucrs/tc2_microservico_ccs/api/CcsApi.java
+++ src/main/java/com/carlosruanpucrs/tc2_microservico_ccs/api/CcsApi.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class CcsApi {
     private final CcsService ccsService;
 
     @PostMapping("/notificacao/abertura")
-    public ResponseEntity<Void> notificarAberturaConta(AberturaContaNotificationCcsRequest request){
+    public ResponseEntity<Void> notificarAberturaConta(@RequestBody AberturaContaNotificationCcsRequest request){
         ccsService.notificarAberturaConta(request);
         return ResponseEntity.ok().build();
     }
