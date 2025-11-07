7d40962958eb437762aae59cb06e2a56c2051850
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-11-07 06:15:04 -0300

    refactor

--- src/main/java/com/carlosruanpucrs/tc2_microservico_ccs/Tc2MicroservicoCcsApplication.java
+++ src/main/java/com/carlosruanpucrs/tc2_microservico_ccs/Tc2MicroservicoCcsApplication.java
@@ -2,7 +2,9 @@ package com.carlosruanpucrs.tc2_microservico_ccs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Tc2MicroservicoCcsApplication {
 
